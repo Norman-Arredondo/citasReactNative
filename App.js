546c2716d05451588e5d56836ab20c6926b3fbdf
Norman Arredondo <arredondo_norman25@outlook.es>
2023-05-24 17:39:00 -0600

    Styles
    Estilos a nuestros componentes
    Flex

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 import React, { Fragment } from 'react';
 
 import {
-  SafeAreaView,
+  View,
   Text,
   StyleSheet
 } from 'react-native';
@@ -10,23 +10,32 @@ import {
 const App = () => {
   return (
 
-    <SafeAreaView>
+    <View style={styles.container}>
       <Text style={styles.titulo}>Administrador de Citas {''}
-        <Text>Veterinaria</Text>
+        <Text style={styles.tituloBold}>Veterinaria</Text>
       </Text>
 
-    </SafeAreaView>
+    </View>
 
   );
 };
 
 /**método .create para crear la hoja de estilos, dentro un objeto con todas las instrucciones CSS*/
 const styles = StyleSheet.create({
+
+  container: {
+    backgroundColor: '#F3F4F6',
+    flex: 1
+  },
   titulo: {
     textAlign:'center',
     fontSize: 30,
     color: '#374151',
     fontWeight: '600'
+  },
+  tituloBold: {
+    fontWeight: '900',
+    color: '#6D28D9',
   }
 });
 
