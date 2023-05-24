48dc74e927ba4ab976faee9dc88f643873964430
Norman Arredondo <arredondo_norman25@outlook.es>
2023-05-24 01:03:28 -0600

    Hello world
    Componentes

---
+++ App.js
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    /** Funciona para los dispositivos con Noch */
+    <SafeAreaView>
+      <Text>Hola Mundo</Text>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
