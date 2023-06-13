1265b67ef8f3ceab61aecab9ce92a0068eab7ca2
Norman Arredondo <arredondo_norman25@outlook.es>
2023-06-13 02:41:23 -0600

    Creando el State
    State para cada TextInput

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Modal, Text, SafeAreaView, StyleSheet, TextInput, View, ScrollView } from 'react-native'
 
 export const Formulario = ({ modalVisible }) => {
+
+    const [paciente, setPaciente] = useState('');
+    const [propietario, setPropietario] = useState('');
+    const [email, setEmail] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [sintomas, setSintomas] = useState('');
+
+
     return (
         <Modal animationType='slide' visible={modalVisible}>
             <SafeAreaView style={styles.contenido}>
@@ -12,27 +20,33 @@ export const Formulario = ({ modalVisible }) => {
 
                     <View style={styles.campo}>
                         <Text style={styles.label}>Nombre del Paciente</Text>
-                        <TextInput style={styles.input} placeholder='Nombre del Paciente' placeholderTextColor={'#666'} />
+                        <TextInput style={styles.input} placeholder='Nombre del Paciente' placeholderTextColor={'#666'} value={paciente}
+                                    onChangeText={setPaciente}
+                        />
                     </View>
 
                     <View style={styles.campo}>
                         <Text style={styles.label}>Nombre del Propietario</Text>
-                        <TextInput style={styles.input} placeholder='Nombre del Propietario' placeholderTextColor={'#666'} />
+                        <TextInput style={styles.input} placeholder='Nombre del Propietario' placeholderTextColor={'#666'} value={propietario}
+                                    onChangeText={setPropietario} />
                     </View>
 
                     <View style={styles.campo}>
                         <Text style={styles.label}>Email del Propietario</Text>
-                        <TextInput style={styles.input} placeholder='Email' placeholderTextColor={'#666'} keyboardType='email-address' />
+                        <TextInput style={styles.input} placeholder='Email' placeholderTextColor={'#666'} keyboardType='email-address' value={email}
+                                    onChangeText={setEmail}/>
                     </View>
 
                     <View style={styles.campo}>
                         <Text style={styles.label}>Teléfono del Propietario</Text>
-                        <TextInput style={styles.input} placeholder='Teléfono' placeholderTextColor={'#666'} keyboardType='phone-pad' />
+                        <TextInput style={styles.input} placeholder='Teléfono' placeholderTextColor={'#666'} keyboardType='phone-pad' value={telefono}
+                                    onChangeText={setTelefono} maxLength={10}/>
                     </View>
 
                     <View style={styles.campo}>
                         <Text style={styles.label}>Síntomas</Text>
-                        <TextInput style={styles.input} placeholder='Síntomas del paciente' placeholderTextColor={'#666'} />
+                        <TextInput style={[styles.input, styles.sintomasInput]} placeholder='Síntomas del paciente' placeholderTextColor={'#666'} value={sintomas}
+                                    onChangeText={setSintomas} multiline={true} numberOfLines={4}/>
                     </View>
 
 
@@ -75,5 +89,8 @@ const styles = StyleSheet.create({
         marginTop: 10,
         marginHorizontal: 30,
         marginBottom: 10
+    },
+    sintomasInput: {
+        height: 100
     }
 });
