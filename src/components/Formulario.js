13adfc4bd43f3ffae7173e30d4c4f6c566b392c5
Norman Arredondo <arredondo_norman25@outlook.es>
2023-06-13 02:21:34 -0600

    TextInput
    ScrollView

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,21 +1,42 @@
 import React from 'react'
-import { Modal, Text, SafeAreaView, StyleSheet, TextInput, View} from 'react-native'
+import { Modal, Text, SafeAreaView, StyleSheet, TextInput, View, ScrollView } from 'react-native'
 
 export const Formulario = ({ modalVisible }) => {
     return (
         <Modal animationType='slide' visible={modalVisible}>
-            <SafeAreaView style = {styles.contenido}>
-                <Text style = {styles.titulo}>Nueva {''}
-                    <Text style = {styles.tituloBold}>Cita</Text>
-                </Text>
+            <SafeAreaView style={styles.contenido}>
+                <ScrollView>
+                    <Text style={styles.titulo}>Nueva {''}
+                        <Text style={styles.tituloBold}>Cita</Text>
+                    </Text>
 
-                <View style= {styles.campo}>
-                    <Text style= {styles.label}>Nombre Paciente</Text>
-                    <TextInput style= {styles.input} placeholder='Nombre del Paciente' placeholderTextColor={'#666'}/>
-                </View>
-               
+                    <View style={styles.campo}>
+                        <Text style={styles.label}>Nombre del Paciente</Text>
+                        <TextInput style={styles.input} placeholder='Nombre del Paciente' placeholderTextColor={'#666'} />
+                    </View>
 
-                
+                    <View style={styles.campo}>
+                        <Text style={styles.label}>Nombre del Propietario</Text>
+                        <TextInput style={styles.input} placeholder='Nombre del Propietario' placeholderTextColor={'#666'} />
+                    </View>
+
+                    <View style={styles.campo}>
+                        <Text style={styles.label}>Email del Propietario</Text>
+                        <TextInput style={styles.input} placeholder='Email' placeholderTextColor={'#666'} keyboardType='email-address' />
+                    </View>
+
+                    <View style={styles.campo}>
+                        <Text style={styles.label}>Teléfono del Propietario</Text>
+                        <TextInput style={styles.input} placeholder='Teléfono' placeholderTextColor={'#666'} keyboardType='phone-pad' />
+                    </View>
+
+                    <View style={styles.campo}>
+                        <Text style={styles.label}>Síntomas</Text>
+                        <TextInput style={styles.input} placeholder='Síntomas del paciente' placeholderTextColor={'#666'} />
+                    </View>
+
+
+                </ScrollView>
             </SafeAreaView>
         </Modal>
     )
@@ -39,8 +60,7 @@ const styles = StyleSheet.create({
     input: {
         backgroundColor: '#fff',
         padding: 15,
-        borderRadius: 10,
-        marginBottom: 15
+        borderRadius: 10
 
     },
     label: {
